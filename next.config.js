0af634050b93055bb96cbbbd537411d4885460ab
talibabtou <talibabtou@gmail.com>
2025-08-03 23:33:48 +0200

    fix: small changes and starting warning solved

--- next.config.js
+++ next.config.js
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
   },
   images: {
     domains: [],
   },
   env: {
-    CUSTOM_KEY: process.env.CUSTOM_KEY,
+    // Add any environment variables here
   },
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
